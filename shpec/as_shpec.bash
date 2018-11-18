2712c27cdb9ccb5b28501f19498e50adb7390312
Ted Lilley <ted.lilley@gmail.com>
2018-11-18 22:09:13 +0000

    source-as

--- shpec/as_shpec.bash
+++ shpec/as_shpec.bash
@@ -4,11 +4,10 @@ set -o noglob
 export TMPDIR=${TMPDIR:-$HOME/tmp}
 mkdir -p $TMPDIR
 
-set --
 Dir=$(dirname $(readlink -f $BASH_SOURCE))/..
 source $Dir/lib/shpec-helper.bash
 cd $Dir/lib
-source ./module ''
+source ./as library
 
 in? () {
   [[ $IFS$1$IFS == *"$IFS$2$IFS"* ]]
@@ -52,27 +51,27 @@ END
   ti
 end_describe
 
-describe module
+describe 'as module'
   alias setup='dir=$(mktemp -d) || return'
   alias teardown='rm -rf $dir'
 
   it "stores a module in a global hash"
     touch $dir/sample.bash
-    source module $dir/sample.bash
+    source ./as module $dir/sample.bash
     [[ -v _modules_[$dir/sample.bash] ]]
     assert equal 0 $?
   ti
 
   it "sources the file"
     echo 'echo hello' >$dir/sample.bash
-    result=$(source module $dir/sample.bash)
+    result=$(source ./as module $dir/sample.bash)
     assert equal hello "$result"
   ti
 
   it "imports a function"
     echo 'foo () { :;}' >$dir/sample.bash
     result=$(env -i bash <<END
-      source module $dir/sample.bash
+      source ./as module $dir/sample.bash
       compgen -A function | egrep -v '^_.*_$|^module.already_loaded$'
 END
     )
@@ -81,14 +80,14 @@ END
 
   it "doesn't leave function aliases"
     echo 'foo () { :;}' >$dir/sample.bash
-    source module $dir/sample.bash
+    source ./as module $dir/sample.bash
     ! alias foo &>/dev/null
     assert equal 0 $?
   ti
 
   it "leaves defined aliases"
     echo 'alias foo=:' >$dir/sample.bash
-    source module $dir/sample.bash
+    source ./as module $dir/sample.bash
     alias foo &>/dev/null
     assert equal 0 $?
   ti
@@ -96,10 +95,10 @@ END
   it "leaves defined aliases which overlap a sub-source"
     cat <<'    END' >$dir/sample.bash
       alias foo=:
-      source module $dir/sample2.bash
+      source ./as module $dir/sample2.bash
     END
     echo 'foo () { :;}' >$dir/sample2.bash
-    source module $dir/sample.bash
+    source ./as module $dir/sample.bash
     alias foo &>/dev/null
     assert equal 0 $?
   ti
@@ -109,23 +108,23 @@ END
       foo () { bar        ;}
       bar () { echo hello ;}
     END
-    source module $dir/sample.bash
+    source ./as module $dir/sample.bash
     result=$(sample.foo)
     assert equal hello $result
   ti
 
   it "allows modules to import other modules"
     cat >$dir/foo.bash <<END
-      source module $dir/bar.bash
+      source ./as module $dir/bar.bash
       bat () { :;}
 END
     cat >$dir/bar.bash <<END
-      source module $dir/baz.bash
+      source ./as module $dir/baz.bash
       bat () { :;}
 END
     echo 'bat () { :;}' >$dir/baz.bash
     result=$(env -i bash <<END
-      source module $dir/foo.bash
+      source ./as module $dir/foo.bash
       compgen -A function | egrep -v '^_.*_$|^module.already_loaded$'
 END
     )
@@ -137,7 +136,7 @@ END
     echo 'foo () { :;}' >$dir/sample1.bash
     echo 'foo () { :;}' >$dir/sample2.bash
     result=$(env -i bash <<END
-      source module $dir/sample1.bash $dir/sample2.bash
+      source ./as module $dir/sample1.bash $dir/sample2.bash
       compgen -A function | egrep -v '^_.*_$|^module.already_loaded$'
 END
     )
@@ -146,7 +145,7 @@ END
   ti
 
   it "stores a module in a global hash"
-    echo "source $Dir/lib/module" >$dir/sample.bash
+    echo "source ./as module" >$dir/sample.bash
     source $dir/sample.bash
     [[ -v _modules_[$dir/sample.bash] ]]
     assert equal 0 $?
@@ -154,7 +153,7 @@ END
 
   it "imports a function"
     cat <<END >$dir/sample.bash
-      source $Dir/lib/module
+      source ./as module
       module.already_loaded && return
       foo () { :;}
 END
@@ -169,7 +168,7 @@ END
   it "lets the module be assigned a different name"
     echo 'foo () { :;}' >$dir/sample.bash
     result=$(env -i bash <<END
-      source module sam=$dir/sample.bash
+      source ./as module sam=$dir/sample.bash
       compgen -A function | egrep -v '^_.*_$|^module.already_loaded$'
 END
     )
@@ -178,15 +177,36 @@ END
 
   it "lets the module be assigned a different name when its a module already"
     cat <<END >$dir/sample.bash
-      source $Dir/lib/module
+      source ./as module
       module.already_loaded && return
       foo () { :;}
 END
     result=$(env -i bash <<END
-      source module sam=$dir/sample.bash
+      source ./as module sam=$dir/sample.bash
       compgen -A function | egrep -v '^_.*_$|^module.already_loaded$'
 END
     )
     assert equal sam.foo "$result"
+  ti
+
+  it "lets a module include another module when sourced normally"
+    cat <<END >$dir/foo.bash
+      source ./as module
+      module.already_loaded && return
+
+      source ./as module bar=$dir/bar.bash
+END
+    cat <<END >$dir/bar.bash
+      source ./as module
+      module.already_loaded && return
+
+      foo () { :;}
+END
+    result=$(env -i bash <<END
+      source $dir/foo.bash
+      compgen -A function | egrep -v '^_.*_$|^module.already_loaded$'
+END
+    )
+    assert equal foo.bar.foo "$result"
   ti
 end_describe
